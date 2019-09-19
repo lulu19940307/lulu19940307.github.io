3c21fc72700f2a793a509d90a3b7ad6ed794232f
lulu19940307 <lulu19940307@163.com>
2019-09-19 15:37:11 +0800

    Site updated: 2019-09-19 15:37:11

--- dist/music.js
+++ dist/music.js
@@ -11,7 +11,7 @@ const ap = new APlayer({
             artist: '王贰浪',
             //url: 'http://m10.music.126.net/20190918174230/b8d9cd234ec1e82bd70c6974834be2cb/ymusic/555b/545f/005f/b193f4fbc9c5c3a94eb17531a71fbc26.mp3',
             //url:'https://music.163.com/song?id=1331819951',
-            url:'http://m10.music.126.net/20190918184013/8598de4fcecce2f3b2cfb9878d93a8fc/ymusic/555b/545f/005f/b193f4fbc9c5c3a94eb17531a71fbc26.mp3',
+            url:'http://m10.music.126.net/20190919155657/3c9e5490ea34c1beac53762750038d7a/ymusic/555b/545f/005f/b193f4fbc9c5c3a94eb17531a71fbc26.mp3',
             //cover: 'img/cover1.jpg', 
             cover:'http://p1.music.126.net/ejEPGN6ulPSgCBXGq7dgqw==/109951163720047382.jpg?param=130y130'
             
