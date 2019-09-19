01b69c95f9682304b93eea3acecdd317125a0f67
lulu19940307 <lulu19940307@163.com>
2019-09-19 11:03:10 +0800

    Site updated: 2019-09-19 11:03:10

--- dist/music.js
+++ dist/music.js
@@ -12,7 +12,8 @@ const ap = new APlayer({
             //url: 'http://m10.music.126.net/20190918174230/b8d9cd234ec1e82bd70c6974834be2cb/ymusic/555b/545f/005f/b193f4fbc9c5c3a94eb17531a71fbc26.mp3',
             //url:'https://music.163.com/song?id=1331819951',
             url:'http://m10.music.126.net/20190918184013/8598de4fcecce2f3b2cfb9878d93a8fc/ymusic/555b/545f/005f/b193f4fbc9c5c3a94eb17531a71fbc26.mp3',
-            cover: 'cover1.jpg', 
+            cover: 'img/cover1.jpg', 
+            //cover:'http://p1.music.126.net/ejEPGN6ulPSgCBXGq7dgqw==/109951163720047382.jpg?param=130y130'
             
         }]
 });
